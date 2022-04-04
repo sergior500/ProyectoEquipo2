66d2a93eab9476c6cbadb111bf023c41640dccfb
Estudiante <estudiante@01DAW01>
2022-04-04 12:24:36 +0200

    Creacion Historial y Test

--- src/main/java/com/model/Historial.java
+++ src/main/java/com/model/Historial.java
@@ -1,6 +1,44 @@
 package main.java.com.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Historial {
+	List<PaginaWeb> historial = new ArrayList<PaginaWeb>();
 	
+	public Historial() {
+		
+	}
 	
+	public List<PaginaWeb> getHistorial() {
+		return historial;
+	}
+
+	public void setHistorial(List<PaginaWeb> historial) {
+		this.historial = historial;
+	}
+
+	public void annadirAlHistorial(PaginaWeb pg1) {
+		historial.add(pg1);
+	}
+	
+	public void borrarHistorial() {
+		historial.clear();
+	}
+	
+	public List<PaginaWeb> mostrarTodaLaLista() {
+		return historial;
+	}
+	
+	public List<PaginaWeb> mostrarElementoLista(LocalDate fecha) {
+		List<PaginaWeb> resultado = new ArrayList();
+		for (int i=0;i<historial.size();i++) {
+			if(historial.get(i).getFechaYhora().toLocalDate().equals(fecha)){
+				resultado.add(historial.get(i));
+			}
+
+		}
+		return resultado;
+	}
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,21 +1,68 @@
 package test.java;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import main.java.com.model.Historial;
 import main.java.com.model.PaginaWeb;
 
 class Test {
 
-	@org.junit.jupiter.api.Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
 	
 	@org.junit.jupiter.api.Test
 	void testAddPaginas() {
 		PaginaWeb pw1 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
 	}
+	
+	@org.junit.jupiter.api.Test
+	void paginaMismoDiaTest() {
+		List<PaginaWeb> resultado = new ArrayList();
+		PaginaWeb pw1 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
+		PaginaWeb pw2 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
+		resultado.add(pw2);
+		resultado.add(pw1);
+		Historial historial = new Historial();
+		
+		historial.annadirAlHistorial(pw1);
+		historial.annadirAlHistorial(pw2);
 
+		historial.mostrarElementoLista(LocalDate.of(2020, 3, 2));
+		
+		assertTrue(historial.mostrarElementoLista(LocalDate.of(2020, 3, 2)).contains("google"));
+		System.out.println(historial.mostrarElementoLista(LocalDate.of(2020, 3, 2)));
+	}	
+	
+	@org.junit.jupiter.api.Test
+	void borrarListaTest() {
+		List<PaginaWeb> resultado = new ArrayList();
+		Historial h1=new Historial();
+		PaginaWeb pw1 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
+		h1.annadirAlHistorial(pw1);
+		h1.borrarHistorial();
+	
+		assertTrue(h1.getHistorial().isEmpty());
+
+	
+	}
+
+	@org.junit.jupiter.api.Test
+	void mostrarTodaListaTest() {
+		PaginaWeb pw1 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
+		PaginaWeb pw2 = new PaginaWeb("www.facebook.es", LocalDateTime.of(2020,3,2,17,20,00));
+		Historial historial = new Historial();
+		historial.annadirAlHistorial(pw1);
+		historial.annadirAlHistorial(pw2);
+		List<PaginaWeb> resultado = new ArrayList();
+		resultado.add(pw2);
+		resultado.add(pw1);
+		
+		assertEquals(historial.mostrarTodaLaLista(),resultado);
+	
+	}
 }
