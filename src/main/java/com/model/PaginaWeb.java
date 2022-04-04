8d3e640c3425db0a750f6857ceb622c8266e8e90
sergior500 <estudiante@pc50.41700853.41.andared.ced.junta-andalucia.es>
2022-04-04 10:56:38 +0200

    Equals added

--- src/main/java/com/model/PaginaWeb.java
+++ src/main/java/com/model/PaginaWeb.java
@@ -1,14 +1,60 @@
 package main.java.com.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class PaginaWeb {
 	
-	private String nombrePagina;
+	private String nombre;
+	private String URL;
 	private LocalDateTime fechaYhora;
 
 
-	public PaginaWeb(String Nombre, LocalDateTime fechaYhora) {
+	public PaginaWeb(String URL, LocalDateTime fechaYhora) {
+		this.URL = URL;
+		this.fechaYhora = fechaYhora;
+	}
+	public PaginaWeb(String URL, LocalDateTime fechaYhora, String nombre) {
 		
+		this.nombre = nombre;
+		this.URL = URL;
+		this.fechaYhora = fechaYhora;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getURL() {
+		return URL;
+	}
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+	public LocalDateTime getFechaYhora() {
+		return fechaYhora;
+	}
+	public void setFechaYhora(LocalDateTime fechaYhora) {
+		this.fechaYhora = fechaYhora;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(URL, fechaYhora, nombre);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PaginaWeb other = (PaginaWeb) obj;
+		return Objects.equals(fechaYhora, other.fechaYhora);
+	}
+	@Override
+	public String toString() {
+		return "PaginaWeb con nombre:" + nombre + "\nURL:" + URL + "\nFecha Y Hora=" + fechaYhora;
 	}
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,21 @@
 package test.java;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
+
+import main.java.com.model.PaginaWeb;
 
 class Test {
 
 	@org.junit.jupiter.api.Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	
+	@org.junit.jupiter.api.Test
+	void testAddPaginas() {
+		PaginaWeb pw1 = new PaginaWeb("www.google.es", LocalDateTime.of(2020,3,2,17,20,00));
 	}
 
 }
